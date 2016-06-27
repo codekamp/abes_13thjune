aac9df72658844193e0d7d18bd838bada78cdef4
Prashant Chaudhary <101.prashant@gmail.com>
2016-06-27 14:19:28 +0530

    change color of all child windows

--- TheRightWay/src/CodeKampJframe.java
+++ TheRightWay/src/CodeKampJframe.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by cerebro on 23/06/16.
@@ -11,12 +13,13 @@ import java.awt.event.ActionListener;
 public class CodeKampJframe extends JFrame implements ActionListener {
 
 
+    public static int count = 0;
+
     private JButton changeColorButton;
     private JButton duplicateButton;
     private JLabel textLabel;
 
-    private CodeKampJframe[] childWindows = new CodeKampJframe[10];
-    private int childrenCount = 0;
+    private  List<CodeKampJframe> childWindows = new ArrayList<>();
 
     public CodeKampJframe() {
         this("CodeKamp");
@@ -24,6 +27,10 @@ public class CodeKampJframe extends JFrame implements ActionListener {
 
     public CodeKampJframe(String title) {
         super(title);
+
+        CodeKampJframe.count++;
+        this.setTitle("Window number " + CodeKampJframe.count);
+
 
         this.setDefaultCloseOperation(CodeKampJframe.EXIT_ON_CLOSE);
         this.setSize(400, 400);
@@ -47,13 +54,25 @@ public class CodeKampJframe extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.changeColorButton) {
-            Color myCustomColor = new Color(160, 189, 255);
-            this.textLabel.setForeground(myCustomColor);
+
+            this.changeColor();
+
         } else if (e.getSource() == this.duplicateButton) {
             CodeKampJframe frame = new CodeKampJframe();
-            this.childWindows[this.childrenCount] = frame;
-            this.childrenCount++;
+
+            this.childWindows.add(frame);
             frame.setVisible(true);
+        }
+    }
+
+    public void changeColor() {
+
+
+        this.textLabel.setForeground(Color.red);
+
+        for (int i = 0; i < this.childWindows.size(); i++) {
+            CodeKampJframe childWindow = this.childWindows.get(i);
+            childWindow.changeColor();
         }
     }
 }
